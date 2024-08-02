fed243d502285db5ea03166f50d1f5b6c473426c
PunthiraChin <164843847+PunthiraChin@users.noreply.github.com>
2024-08-02 14:46:01 +0700

    Update testA2.js

--- testA2.js
+++ testA2.js
@@ -1,19 +1,22 @@
-function findSecondBiggest(inputArr) {
+function secondMax(inputArr) {
   if (inputArr.length === 0)
     return console.log("Please input array with number");
-  let biggest = inputArr[0];
-  let secondBiggest = biggest;
-  console.log("running the function");
+  let biggest = -Infinity;
+  let secondBiggest = -Infinity;
   for (let i = 0; i < inputArr.length; i++) {
     if (inputArr[i] >= biggest) {
       secondBiggest = biggest;
       biggest = inputArr[i];
+    } else if (inputArr[i] >= secondBiggest) {
+      secondBiggest = inputArr[i];
     }
   }
   return secondBiggest;
 }
 // Test cases
-findSecondBiggest([]);
-findSecondBiggest([-1, 4, 30, 2, -4]);
-findSecondBiggest([3, 4, 5, 6, 7]);
-findSecondBiggest([3, 4, 5, 6, 7, 7]);
+secondMax([]);
+secondMax([-1, 4, 30, 2, -4]);
+secondMax([3, 4, 5, 6, 7]);
+secondMax([3, 4, 5, 6, 7, 7]);
+secondMax([7, 6, 5, 4]);
+secondMax([7, 7, 5, 4]);
